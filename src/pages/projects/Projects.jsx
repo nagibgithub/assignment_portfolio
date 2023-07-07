0ad307f1079e64f9322d73dadcad0a3a20b360cf
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-07-07 20:13:54 +0600

    customised home photo

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,13 +1,20 @@
-import ChefRecipe from "./ChefRecipe";
-import LegoToy from "./LegoToy";
-import SummerCamp from "./SummerCamp";
+import { useEffect, useState } from "react";
+import ProjectsLayout from "./ProjectsLayout";
 
 const Projects = () => {
+
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        fetch('/projectsData.json').then(res => res.json()).then(data => setProjects(data));
+    }, []);
+
+    console.log(projects);
+
     return (
-        <div>
-            <SummerCamp></SummerCamp>
-            <LegoToy></LegoToy>
-            <ChefRecipe></ChefRecipe>
+        <div className="md:grid md:grid-cols-3 gap-5 container mx-auto">
+            {
+                projects.map((pd, index) => <ProjectsLayout key={index} project={pd}></ProjectsLayout>)
+            }
         </div>
     );
 };
--- src/pages/projects/ProjectsLayout.jsx
+++ src/pages/projects/ProjectsLayout.jsx
@@ -1,17 +1,23 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-const ProjectsLayout = () => {
+import Atropos from 'atropos/react';
+
+const ProjectsLayout = ({ project }) => {
+
+    const { name, live, client_code, server_code, img_home, img_list } = project
+    const webPageImages = img_list.map((pd, index) => <div key={index} className="border-2 p-5"><img className="w-32" src={pd} alt="page" /></div>);
+
     return (
-        <div>
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-            </Swiper>
+        <div className="flex flex-col justify-center items-center my-10 border-2 border-sky-500 rounded-2xl p-5 shadow-lg shadow-sky-600">
+            <Atropos shadow={false} className='cursor-pointer'>
+                <img src={img_home} alt="Web Site Home Page" />
+                {/* <ModalImage small={img_home} large={img_home} alt="Site Home" />; */}
+            </Atropos>
+            <h1>{name}</h1>
+            <a href={live}><button>Live Link</button></a>
+            <a href={client_code}><button>Client Side Code</button></a>
+            <a href={server_code}><button>Server Side Code</button></a>
+            <div className="grid grid-cols-3">
+                {webPageImages}
+            </div>
         </div>
     );
 };
